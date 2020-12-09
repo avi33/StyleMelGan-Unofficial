aa85ddd6f107f0423020ab510099e1822db6b805
avig <avi.gazneli@gmail.com>
2020-12-09 22:12:29 +0200

    remove file

--- run_train_G.sh
+++
@@ -1,6 +0,0 @@
-python ./train_G.py \
---batch_size 32 \
---data_path /home/avig/data/speech/youtube/dataset \
---epochs 3000 \
---save_path /home/avig/data/speech/logs/youtube/inv-smg \
---load_path /home/data/speech/logs/youtube/inv-smg
--- run_train_GD.sh
+++
@@ -1,6 +0,0 @@
-python ./train_GD.py \
---batch_size 32 \
---data_path /home/avig/data/speech/youtube/dataset \
---epochs 3000 \
---save_path /home/avig/data/speech/logs/youtube/inv-smg-GD \
---Gpath /home/avig/data/speech/logs/youtube/inv-smg
